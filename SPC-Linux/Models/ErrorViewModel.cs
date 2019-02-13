62ad6ea63fe2403101738dbd2028d81a61447284
Eldhose Paul <eldhose.paul@sreegcloudgmail.onmicrosoft.com>
2019-02-13 10:22:09 +0530

    First Commit

---
+++ SPC-Linux/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SPC_Linux.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
